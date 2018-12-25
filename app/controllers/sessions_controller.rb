a552689020a2f9bc8e4b90600e19401a59ad308c
SMartinSci <sm674@cornell.edu>
2018-12-25 10:55:14 -0800

    Add session create, new, destroy and omniauth methods and update routes with login sessions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+    skip_before_action :require_login, only: [:new, :create, :facebook]
+    
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:session][:email])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:notice] = "Login failed, email/password can't be blank."
+            redirect_to login_path
+        end
+    end
+
+
+    #Need to add def facebook: omniauth login method
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
 end
