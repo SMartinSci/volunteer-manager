ce78418c7a609de39f4a6264172dfced98b90a62
SMartinSci <sm674@cornell.edu>
2018-12-25 09:42:35 -0800

    Add model relationships to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,11 @@
 class Project < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    validates :time, presence: true
+    validates :location, presence: true
+    validates :time, presence: true
+    validates :description, presence: true
 
-    has_many :users 
+    has_many :roles
     has_many :tasks 
+    has_many :users, through: :roles
 end
