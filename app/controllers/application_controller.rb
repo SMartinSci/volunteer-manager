28b436bc9052afac75e1ec814341ff6b9d34ca10
SMartinSci <sm674@cornell.edu>
2018-12-29 11:32:45 -0800

    Add application controller log in, current user, and require login methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+
+private
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:message] = "Log in to access this section!"
+      redirect_to login_path
+    end
+  end
 end
