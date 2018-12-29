e87d3df48cce5b8a610904be5c3eadeaa6d55b92
SMartinSci <sm674@cornell.edu>
2018-12-29 14:05:18 -0800

    Add routes to config routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'welcome#index'
     
+    resources :users
+    resources :projects do
+      resources :roles do
+        resources :tasks
+    end
+
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
     delete '/logout'  => 'sessions#destroy'
