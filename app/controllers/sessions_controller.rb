d38e79ae53fda2c978ecea45232b1b149a80f0f9
SMartinSci <sm674@cornell.edu>
2018-12-30 19:38:23 -0800

    Slight edit to syntax in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-        render :new
+            render :new
         end
     end
 
