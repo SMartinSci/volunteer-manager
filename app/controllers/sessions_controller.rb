59e266257b591e2e938d95d41ab62448b12e946c
SMartinSci <sm674@cornell.edu>
2018-12-30 20:07:13 -0800

    Syntax change sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(email: params[:session][:email])
-        if @user && @user.authenticate(params[:session][:password])
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
