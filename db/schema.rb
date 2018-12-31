751b9683e047f0bee24cc67cc4ccaa78c36ccd18
SMartinSci <sm674@cornell.edu>
2018-12-30 19:51:37 -0800

    Add omniauth methods to users table due to facebook authentication method not working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_25_131650) do
+ActiveRecord::Schema.define(version: 2018_12_31_034555) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2018_12_25_131650) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "provider"
+    t.string "uid"
   end
 
 end
