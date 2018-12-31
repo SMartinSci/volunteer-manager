4adaf3d3173b7bbf356568f7dab0ab4d28b4c761
SMartinSci <sm674@cornell.edu>
2018-12-31 04:50:36 -0800

    No change to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(name: params[:user][:name])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
+            binding.pry
             redirect_to user_path(@user)
         else
             flash[:notice] = "Login failed, email/password can't be blank."
