779ce042ce58b16b4061df1289b91534f44250ab
SMartinSci <sm674@cornell.edu>
2019-01-16 09:47:44 -0800

    Removed user models and migrations and generated devise model and migrated the revised database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_31_034555) do
+ActiveRecord::Schema.define(version: 2019_01_16_172607) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -36,13 +36,23 @@ ActiveRecord::Schema.define(version: 2018_12_31_034555) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "provider"
     t.string "uid"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
