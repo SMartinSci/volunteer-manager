6aea5dbc49c717d84caacad89c949d6061cd24a3
SMartinSci <sm674@cornell.edu>
2019-01-17 15:35:02 -0800

    Revert "Removed user models and migrations and generated devise model and migrated the revised database"
    This reverts commit 779ce042ce58b16b4061df1289b91534f44250ab.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_secure_password
+    has_secure_password
       
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, :confirmation => true
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, :confirmation => true
 
-  has_many :roles
-  has_many :projects, through: :roles
+    has_many :roles
+    has_many :projects, through: :roles
 
-  def self.find_or_create_by_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password_digest = SecureRandom.hex
-      user.name = auth.info.name
-      user.save!
+    def self.find_or_create_by_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password_digest = SecureRandom.hex
+        user.name = auth.info.name
+        user.save!
     end
   end 
 end
---
+++ db/migrate/20181220141811_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+      t.string :name
+    end
+  end
+end
