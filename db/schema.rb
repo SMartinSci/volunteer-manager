12a59fea5ccc2aa953a19abbccba65e65142f9ab
SMartinSci <sm674@cornell.edu>
2019-01-20 16:13:29 -0800

    Remove unnecessary columns from users table and remove delete link on projects show page to limit user abilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_000413) do
+ActiveRecord::Schema.define(version: 2019_01_21_001150) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -41,9 +41,6 @@ ActiveRecord::Schema.define(version: 2019_01_18_000413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "provider"
-    t.string "uid"
-    t.boolean "admin", default: false
   end
 
 end
